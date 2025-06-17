faf372aeb2075a01a5b328a1d72807b897fe8884
TogetherHumanity <DAVID@TOGETHERHUMANITY.COM>
2025-06-16 20:30:16 -0700

    Delete src directory

--- src/main.tsx
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import AppRoutes from './AppRoutes';
-ReactDOM.createRoot(document.getElementById('root')).render(<AppRoutes />);
