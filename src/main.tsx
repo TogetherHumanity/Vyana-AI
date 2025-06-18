2f5d96ef83db5e2bafcb489d7c23d28f6c1d26cc
TogetherHumanity <DAVID@TOGETHERHUMANITY.COM>
2025-06-17 20:49:49 -0700

    Add files via upload

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <div className="text-lg text-center text-gray-700 mt-20">Hello from Vyana AI!</div>
+  </React.StrictMode>
+);
