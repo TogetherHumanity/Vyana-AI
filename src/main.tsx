ec53165153f7a81785b71db549fcdbcd05cbd893
TogetherHumanity <DAVID@TOGETHERHUMANITY.COM>
2025-06-17 22:07:33 -0700

    Delete src directory

--- src/main.tsx
+++
@@ -1,9 +0,0 @@
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <div className="text-lg text-center text-gray-700 mt-20">Hello from Vyana AI!</div>
-  </React.StrictMode>
-);
